0088a3b6a66725e64b3d2170d8a836bbc8ff5e76
Adiam Ahmed <adu.adu911@gmail.com>
2024-09-05 13:23:14 -0400

    added overview to the landing page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import '../src/styles/global.scss'
 import '../src/components/Header/Header'
 import Header from '../src/components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Overview from './components/Overview/Overview';
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Overview />
     </div>
   );
 }
