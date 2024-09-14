29fbf4acc9aa945f83e95e14ddd6508010655e41
Adiam Ahmed <adu.adu911@gmail.com>
2024-09-14 06:23:20 -0400

    add new images to cards

--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Card.scss'
 
-const Card = ({ image, text }) => {
+const Card = ({ image, text, paragraph }) => {
   return (
       <div className="card">
           <img src={image} alt="Card" className="card__image" />
           <div className="card__overlay">
-              <p className="card__text">{text}</p>
+              <h3 className="card__text">{text}</h3>
+              <p className="card__paragraph"> {paragraph}</p>
           </div>
       </div>
   )
