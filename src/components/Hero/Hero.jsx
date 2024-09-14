fe9d7a4124b981588bc4e5128c043abcc149f324
Adiam Ahmed <adu.adu911@gmail.com>
2024-09-14 06:21:42 -0400

    modify hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="hero__content">
-                <h1 className="hero__title">Start Your Journey Now.</h1>
+                <h1 className="hero__title">Rooted in Community, Routed to Success.</h1>
                 <h3 className="hero__subtitle">We’re here to help you settle in, connect, and thrive in your new life.</h3>
                 <CTAButton
                     className={`header`}
